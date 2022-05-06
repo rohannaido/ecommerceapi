d9f00ac16a2103c5eb1d3fe86eb4b7d31a9b5a36
rohannaido <87189343+rohannaido@users.noreply.github.com>
2022-05-06 10:35:57 +0530

    feat: weight and flavour for product collection

--- models/Product.js
+++ models/Product.js
@@ -6,8 +6,8 @@ const ProductSchema = mongoose.Schema(
         desc: { type: String, required: true },
         img: { type: String, required: true },
         categories: { type: Array },
-        size: { type: Array },
-        color: { type: Array },
+        weight: { type: Array },
+        flavour: { type: Array },
         price: { type: Number, required: true },
         inStock: { type: Boolean, default: true }
     },
